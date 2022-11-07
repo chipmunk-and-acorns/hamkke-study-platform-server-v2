de05d304cf45103534559208e9b5d52deeff554a
chipmunk-dev <devquokkajeong@gmail.com>
2022-11-07 22:13:39 +0900

    feat: #1 domain 설계
    article과 article_comment 도메인 설계

---
+++ src/main/java/com/hamkke/hamkkestudyplatformserverv2/domain/Article.java
@@ -0,0 +1,15 @@
+package com.hamkke.hamkkestudyplatformserverv2.domain;
+
+import java.time.LocalDateTime;
+
+public class Article {
+    private long id;
+    private String title;
+    private String content; // 제목
+    private String hashtag; // 내용
+
+    private LocalDateTime createdAt; // 생성일시
+    private String createdBy; // 생성자
+    private LocalDateTime modifiedAt; // 수정일시
+    private String modifiedBy; // 수정지
+}
