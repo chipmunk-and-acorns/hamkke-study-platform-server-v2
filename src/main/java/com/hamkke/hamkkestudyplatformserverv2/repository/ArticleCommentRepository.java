c3dc09acf3ad98c7d8f44cf5feb8779fed89f898
chipmunk <107650362+chipmunk-dev@users.noreply.github.com>
2022-11-10 00:57:05 +0900

    게시판 API 검색 기능 구현 (#10)
    * feat: #9 - Spring Data Rest 테스트 제외 처리
    
    Spring Data Rest 테스트는 불 필요함으로 Disabled로 처리함
    
    * feat: #9 - querydsl 디펜던시 추가 및 gitignore 추가
    
    querydsl을 사용할 수 있는 환경을 구성
    
    * feat: #9 - Article, Article Comment searchAPI 구현
    
    * Article
    - title, content, hashtag, createdAt, createdBy 검색 API 적용
    * Article_Comment
    - content, createdAt, createdBy 검색 API 적용

--- src/main/java/com/hamkke/hamkkestudyplatformserverv2/repository/ArticleCommentRepository.java
+++ src/main/java/com/hamkke/hamkkestudyplatformserverv2/repository/ArticleCommentRepository.java
@@ -1,9 +1,28 @@
 package com.hamkke.hamkkestudyplatformserverv2.repository;
 
 import com.hamkke.hamkkestudyplatformserverv2.domain.ArticleComment;
+import com.hamkke.hamkkestudyplatformserverv2.domain.QArticle;
+import com.hamkke.hamkkestudyplatformserverv2.domain.QArticleComment;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+public interface ArticleCommentRepository extends
+        JpaRepository<ArticleComment, Long>,
+        QuerydslPredicateExecutor<ArticleComment>,
+        QuerydslBinderCustomizer<QArticleComment>
+{
+    @Override
+    default void customize(QuerydslBindings bindings, QArticleComment root) {
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.content, root.createdBy, root.createdBy);
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    };
 }
