9a03efcde68145afe7219453903ac40d54ffcb98
chipmunk-dev <devquokkajeong@gmail.com>
2022-11-09 22:53:18 +0900

    feat: #7 - Data rest 설정
    게시글, 댓글의 json api를 자동으로 restful하게 만들어지게 설정

--- src/main/java/com/hamkke/hamkkestudyplatformserverv2/repository/ArticleRepository.java
+++ src/main/java/com/hamkke/hamkkestudyplatformserverv2/repository/ArticleRepository.java
@@ -2,7 +2,8 @@ package com.hamkke.hamkkestudyplatformserverv2.repository;
 
 import com.hamkke.hamkkestudyplatformserverv2.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
-
 }
